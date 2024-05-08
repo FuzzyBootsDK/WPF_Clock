fbd0b2f9a8fd017290b139d8bceb99814165f006
FuzzyBootsDK <98462045+FuzzyBootsDK@users.noreply.github.com>
2024-05-08 11:02:53 +0200

    Initial commit of WPF Clock project
    This commit includes the creation of a .NET 8.0 WPF application, WPF_CLock, with files such as MainWindow.xaml, App.xaml, and their respective CS files. The application includes a basic clock functionality that updates every second. Associated files used for building, debugging, and running the project are also included.
    
    Signed-off-by: FuzzyBootsDK <98462045+FuzzyBootsDK@users.noreply.github.com>

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace WPF_CLock;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+        DispatcherTimer timer = new DispatcherTimer();
+        timer.Interval = TimeSpan.FromSeconds(1);
+        timer.Tick += (s, a) => 
+        {
+            TimeTextBox.Text = DateTime.Now.ToString("hh:mm:ss");
+            DateTextBox.Text = DateTime.Now.ToString("dd-MM-yyyy");
+            DayTextBox.Text = DateTime.Now.ToString("dddd");
+        };
+        timer.Start();
+    }
+}
