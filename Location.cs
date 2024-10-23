5e1b01da9ebafccab897741e0213e6d2da89e5c5
lasse sorensen <fuzzybootsdk@icloud.com>
2024-10-23 12:23:23 +0200

    Add necessary DLLs and database files
    Included various DLLs for .NET 8.0 along with SQLite libraries and other essential DLLs for the project. Also added several database files and autogenerated code for improved functionality.

---
+++ Location.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_CLock
+{
+    [Table("DMI Stations")]
+    public class Location
+    {
+        [Key]
+        public string stationId { get; set; }
+        public string name { get; set; }
+        public string country { get; set; }
+    }
+}
---
+++ WeatherDbContext.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using DotNetEnv;
+
+namespace WPF_CLock
+{
+    public class WeatherDbContext: DbContext
+    {
+        public DbSet<Location> Locations { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // Load the .env file
+            string appDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            string envFilePath = System.IO.Path.Combine(appDirectory, ".env");
+            string dataBasePath = System.IO.Path.Combine(appDirectory, ".db");
+            Env.Load(envFilePath);
+
+            // Get the database path from the environment variable
+            string dbPath = Env.GetString("DB_PATH");
+
+            // If the environment variable is not set, use the default path
+            if (string.IsNullOrEmpty(dbPath))
+            {
+                dbPath = dataBasePath;
+            }
+
+            // Configure the DbContext to use SQLite with the determined path
+            optionsBuilder.UseSqlite($"Data Source={dbPath}");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Location>().ToTable("DMI Stations"); // Specify the table name using Fluent API
+            modelBuilder.Entity<Location>().HasKey(l => l.stationId);
+        }
+    }
+}
